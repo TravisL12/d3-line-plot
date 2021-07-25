b65bdf4a5690bab274ddc5680e064d94e1e290dc
Travis Lawrence <travis.lawrence12@gmail.com>
2021-07-24 20:57:05 -0700

    clean

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -21,44 +21,72 @@ var svg = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
+const xScale = d3.scaleTime().range([0, width]);
+const xAxis = svg
+  .append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(xScale));
+
+const yScale = d3.scaleLinear().range([height, 0]);
+const yAxis = svg.append("g").attr("class", "y-axis").call(d3.axisLeft(yScale));
+
+// Add a clipPath: everything out of this area won't be drawn.
+svg
+  .append("defs")
+  .append("svg:clipPath")
+  .attr("id", "clip")
+  .append("svg:rect")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("x", 0)
+  .attr("y", 0);
+
+// Create the line variable: where both the line and the brush take place
+const line = svg.append("g").attr("clip-path", "url(#clip)");
+
+// Read the data
 // original: https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv
 d3.csv("public/spending.csv", parseData, operateData);
 
 function parseData(d) {
   const date = d3.timeParse("%m/%d/%Y")(d.date);
   const amount = +d.amount;
-  return { date, amount };
+  return { ...d, date, amount };
+}
+
+function updateLine(duration = 250) {
+  line
+    .select(".line")
+    .transition()
+    .duration(duration)
+    .attr(
+      "d",
+      d3
+        .line()
+        .x((d) => xScale(d.date))
+        .y((d) => yScale(d.amount))
+    );
+}
+
+function setDomains(data) {
+  xScale.domain(d3.extent(data, (d) => d.date));
+  xAxis.transition().call(d3.axisBottom(xScale));
+
+  yScale.domain([0, d3.max(data, (d) => d.amount)]);
+  svg.select(".y-axis").transition().call(d3.axisLeft(yScale));
 }
 
 function operateData(data) {
-  // Add X axis
-  const xScale = d3
-    .scaleTime()
-    .domain(d3.extent(data, (d) => d.date))
-    .range([0, width]);
-  xAxis = svg
-    .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xScale));
+  setDomains(data);
 
-  // Add Y axis
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.amount)])
-    .range([height, 0]);
-  yAxis = svg.append("g").attr("class", "y-axis").call(d3.axisLeft(yScale));
-
-  // Add a clipPath: everything out of this area won't be drawn.
-  svg
-    .append("defs")
-    .append("svg:clipPath")
-    .attr("id", "clip")
-    .append("svg:rect")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("x", 0)
-    .attr("y", 0);
+  // Add the line
+  line
+    .append("path")
+    .data([data])
+    .attr("class", "line")
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5);
 
   // Add brushing
   const brush = d3
@@ -69,72 +97,35 @@ function operateData(data) {
     ])
     .on("end", updateChart);
 
-  // Create the line variable: where both the line and the brush take place
-  const line = svg.append("g").attr("clip-path", "url(#clip)");
-
-  // Add the line
-  line
-    .append("path")
-    .data([data])
-    .attr("class", "line") // I add the class line to be able to modify this line later on.
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr(
-      "d",
-      d3
-        .line()
-        .x((d) => xScale(d.date))
-        .y((d) => yScale(d.amount))
-    );
-
-  // Add the brushing
   line.append("g").attr("class", "brush").call(brush);
-
-  function updateLine(duration = 250) {
-    line
-      .select(".line")
-      .transition()
-      .duration(duration)
-      .attr(
-        "d",
-        d3
-          .line()
-          .x((d) => xScale(d.date))
-          .y((d) => yScale(d.amount))
-      );
-  }
+  updateLine();
 
   function updateChart() {
     extent = d3.event.selection;
 
     if (extent) {
       const [xMin, xMax] = extent.map((val) => restartDay(xScale.invert(val)));
-      xScale.domain([xMin, xMax]);
-
       const yMin = data.findIndex((d) => d.date.getTime() >= xMin.getTime());
       const yMax = data.findIndex((d) => d.date.getTime() >= xMax.getTime());
       const dates = data.filter(
         (d) => d.date >= data[yMin].date && d.date <= data[yMax].date
       );
 
+      xScale.domain([xMin, xMax]);
       yScale.domain([0, d3.max(dates, (d) => d.amount)]);
-      svg.select(".y-axis").transition().call(d3.axisLeft(yScale));
+
+      xAxis.transition().duration(1000).call(d3.axisBottom(xScale));
+      yAxis.transition().call(d3.axisLeft(yScale));
+
       line.select(".brush").call(brush.move, null); // This remove the grey brush area as soon as the selection has been done
     }
 
-    // Update axis and line position
-    xAxis.transition().duration(1000).call(d3.axisBottom(xScale));
     updateLine(1000);
   }
 
   // If user double click, reinitialize the chart
   svg.on("dblclick", () => {
-    xScale.domain(d3.extent(data, (d) => d.date));
-    xAxis.transition().call(d3.axisBottom(xScale));
-
-    yScale.domain([0, d3.max(data, (d) => d.amount)]);
-    svg.select(".y-axis").transition().call(d3.axisLeft(yScale));
+    setDomains(data);
     updateLine();
   });
 }
