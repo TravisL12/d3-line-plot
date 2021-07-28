3189a80108cdc1be3d94857bb8e09610a4f3f6a0
Travis Lawrence <travis.lawrence12@gmail.com>
2021-07-27 22:03:25 -0700

    gitignore

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -9,20 +9,23 @@ export const restartDay = (date) => {
   return date;
 };
 
-d3.csv("public/spending.csv").then((rows) => {
-  const cumData = rows.reduce((acc, d, i) => {
-    const date = d3.timeParse("%m/%d/%Y")(d.date);
-    const amount = i > 0 ? +d.amount + acc[acc.length - 1].amount : +d.amount;
-    return [...acc, { ...d, date, amount }];
-  }, []);
+// original: https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv
+// d3.csv("public/spending.csv").then((rows) => {
+d3.csv(
+  "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv"
+).then((rows) => {
+  // const cumData = rows.reduce((acc, d, i) => {
+  //   const date = d3.timeParse("%m/%d/%Y")(d.date);
+  //   const amount = i > 0 ? +d.amount + acc[acc.length - 1].amount : +d.amount;
+  //   return [...acc, { ...d, date, amount }];
+  // }, []);
 
-  const data = rows
-    .map((d) => {
-      const date = d3.timeParse("%m/%d/%Y")(d.date);
-      const amount = +d.amount;
-      return { ...d, date, amount };
-    })
-    .filter(({ amount }) => amount > 0);
-
+  const data = rows.map((d) => {
+    const date = d3.timeParse("%Y-%m-%d")(d.date);
+    const amount = +d.value;
+    return { date, amount };
+  });
+  // .filter(({ amount }) => amount > 0);
+  console.log(data);
   drawLineChart(data, drawHistogramChart);
 });
